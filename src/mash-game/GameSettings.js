a3ffe5c1400ee86d7d65603f02cdb13764e2d407
IdoAmar <idoamar2412@gmail.com>
2021-11-27 22:25:17 +0200

    - added logic to end the game with lives - unified the game state into a single object - added game settings

---
+++ src/mash-game/GameSettings.js
@@ -0,0 +1,18 @@
+export const easyGameSettings = {
+    emergeInterval = 1000,
+    vanishInterval = 1000,
+    lives = 10,
+}
+export const normalGameSettings = {
+    emergeInterval = 400,
+    vanishInterval = 800,
+    lives = 5,
+}
+export const hardGameSettings = {
+    emergeInterval = 250,
+    vanishInterval = 750,
+    lives = 3
+}
+
+export const smallBoardSize = 3;
+export const largerBoardSize = 5;
--- src/mash-game/MashGame.js
+++ src/mash-game/MashGame.js
@@ -1,14 +1,14 @@
 import './MashGame.css'
-import React , {useState, useEffect, useRef} from 'react';
-const buttonText = 
-`
+import React, { useState, useEffect, useRef } from 'react';
+const buttonText =
+    `
 \u00A0 _\u00A0\u00A0 \u00A0 _\n
 / \\\\..//\\\n
 \u00A0  ( oo)\n  
 \u00A0\u00A0\u00A0\u00A0\u00A0 ° \n
 `
-const clickedButtonText = 
-`
+const clickedButtonText =
+    `
 \u00A0\u00A0 ..\u00A0\u00A0\u00A0\u00A0..\n
 \u00A0\u00A0 \\\\..//\n
 \u00A0  (┬ ┬)\n 
@@ -16,43 +16,48 @@ const clickedButtonText =
 `
 const rows = 3;
 const cols = 3;
+
 function MashGame() {
     const excludeList = useRef([]);
-    const [score, setScore] = useState(0);
-    const incrementScore = () => setScore(score + 1);
-    const [activateIndex, setActivateIndex] = useState();
-    const changeIndex = (shouldClear) => {
-                    if(shouldClear){
-                        setActivateIndex(null);
-                        return;
-                    }
-                    let actualList = [...Array(rows * cols).keys()].filter(e => !excludeList.current.includes(e));
-                    let randomNumber = Math.floor(Math.random() * actualList.length);
-                    setActivateIndex(actualList[randomNumber]);
-    }
+    const [gameState , setGameState] = useState({
+        score : 0,
+        lives: 5,
+        activateIndex: null,
+    })
+
     useEffect(() => {
-        const interval = setInterval(()=>{
-            changeIndex();
-        },300)
+        const interval = setInterval(() => {
+            let actualList = [...Array(rows * cols).keys()].filter(e => !excludeList.current.includes(e));
+            let randomNumber = Math.floor(Math.random() * actualList.length);
+            setGameState(c => ({ ...c, activateIndex : actualList[randomNumber]}));
+        }, 300)
         return () => clearInterval(interval);
     }, [])
-    const boardRows = ([...new Array(rows)]).map((e , i)=> 
-        <CubesRow 
-        key={i}
-        activateIndex={activateIndex} 
-        rowIndex={i}
-        incrementScore={incrementScore}
-        changeIndex={changeIndex}
-        excludeList={excludeList}
+    
+    const boardRows = ([...new Array(rows)]).map((e, i) =>
+        <CubesRow
+            key={i}
+            rowIndex={i}
+            excludeList={excludeList}
+            setGameState={setGameState}
+            gameState={gameState}
         >
         </CubesRow>);
 
     return (
         <div className="MainContent">
-            <div> Score is : {score}</div>
-            <div className="Board">
-                {boardRows}
-            </div>
+
+            {gameState.lives > 0 ?
+                (<div>
+                    <div> Score is : {gameState.score}</div>
+                    <div> Lives left : {gameState.lives}</div>
+                    <div className="Board">
+                        {boardRows}
+                    </div>
+                </div>)
+                :
+                (<div> Your score was : {gameState.score}</div>)
+            }
         </div>
     );
 }
@@ -60,53 +65,59 @@ function MashGame() {
 function CubesRow(props) {
     return (
         <div className="CubeRow">
-            {([...new Array(cols)]).map((e,i) => 
-            <Cube key={props.rowIndex * cols + i}
-            activateIndex={props.activateIndex}
-            index={props.rowIndex * cols + i} 
-            incrementScore={props.incrementScore} 
-            changeIndex={props.changeIndex}
-            excludeList={props.excludeList}
-            >
-            </Cube>)}
+            {([...new Array(cols)]).map((e, i) =>
+                <Cube key={props.rowIndex * cols + i}
+                    index={props.rowIndex * cols + i}
+                    excludeList={props.excludeList}
+                    setGameState={props.setGameState}
+                    gameState={props.gameState}
+                    >
+                </Cube>)}
         </div>
     );
 }
 
 function Cube(props) {
     const [text, setText] = useState('');
-    const [internalIsActive , setInternalIsActive] = useState(false);
-    const isActive = props.activateIndex === props.index;
-
+    const [internalIsActive, setInternalIsActive] = useState(false);
+    const isActive = props.gameState.activateIndex === props.index;
+    const properties = {...props}
     let timeoutRef = useRef(null);
+
+    useEffect((() => () => clearTimeout(timeoutRef.current)),[])
+
     useEffect(() => {
-        if(isActive){
+        if (isActive) {
             setText(buttonText);
             setInternalIsActive(true);
             props.excludeList.current.push(props.index);
             timeoutRef.current = setTimeout(() => {
                 setInternalIsActive(false);
+                props.setGameState(c => ({ ...c, lives : c.lives - 1 }))
                 setText('');
                 props.excludeList.current = [...(props.excludeList.current.filter(e => !(e === props.index)))];
             }, 900);
         }
-    })
+    }, [properties.gameState.activateIndex]);
     return (
-        <div 
-        className={internalIsActive ? 'CubeMashable Cube' : 'Cube'}
-        onClick={() => { 
-            if(internalIsActive){
-                clearTimeout(timeoutRef.current);
-                setText('');
-                setInternalIsActive(false);
-                props.changeIndex(true);
-                props.excludeList.current = [...(props.excludeList.current.filter(e => !(e === props.index)))];
-                props.incrementScore();
-            }
-            }} 
-        onMouseDown={() => {setText(internalIsActive ? clickedButtonText : '')}}
-        onMouseUp={() => {setText(internalIsActive ? buttonText : '')}}
-        onMouseOut={() => {setText(internalIsActive ? buttonText : '')}}>
+        <div
+            className={internalIsActive ? 'CubeMashable Cube' : 'Cube'}
+            onClick={() => {
+                if (internalIsActive) {
+                    clearTimeout(timeoutRef.current);
+                    setText('');
+                    setInternalIsActive(false);
+                    props.setGameState(c => ({ ...c, activateIndex : null }))
+                    props.excludeList.current = [...(props.excludeList.current.filter(e => !(e === props.index)))];
+                    props.setGameState(c => ({ ...c, score : c.score + 1 }));
+                }
+                else {
+                    props.setGameState(c => ({ ...c, lives : c.lives - 1 }))
+                }
+            }}
+            onMouseDown={() => { setText(internalIsActive ? clickedButtonText : '') }}
+            onMouseUp={() => { setText(internalIsActive ? buttonText : '') }}
+            onMouseOut={() => { setText(internalIsActive ? buttonText : '') }}>
             {text}
         </div>);
 }
